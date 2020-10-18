b25ecbb14bd5c4ebf857d197f9a6e0fe8daae7e0
Zen <zen-juen@users.noreply.github.com>
2020-10-18 16:43:38 +0800

    smarter feedback + setup oath function

--- autocalendar.py
+++ autocalendar.py
@@ -1,12 +1,51 @@
 # -*- coding: utf-8 -*-
 from apiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
 import pandas as pd
 import numpy as np
 import pickle
+import os.path
 import dateutil.parser
 from datetime import datetime
+
+
+# =============================================================================
+# Initialize Google API Console Credentials
+# =============================================================================
+
+def setup_oath():
+
+    # Set up credentials
+    scopes = ['https://www.googleapis.com/auth/calendar']
+
+    # Token generated after first time code is run.
+    if os.path.exists('token.pkl'):
+        with open('token.pkl', 'rb') as token:
+            credentials = pickle.load(token)
+
+    # If there are no (valid) credentials available, log in and enter authorization code manually
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+           credentials.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", scopes=scopes)
+            credentials = flow.run_local_server(port=0)
+
+       # Save the credentials for the next run
+        with open('token.pkl', 'wb') as token:
+            pickle.dump(credentials, token)
+
+#    pickle.dump(credentials, open("token.pkl", "wb"))
+#    credentials = pickle.load(open("token.pkl", "rb"))
+    service = build("calendar", "v3", credentials=credentials)
+
+    return service
+
+# =============================================================================
+# Get event information and implement
+# =============================================================================
 
 
 def preprocess_file(file):
@@ -32,6 +71,8 @@ def extract_info(participants, date_col, time_col, location_col,
     activate `filter_column` as the header column to filter by, and `select`
     as the cell entries in this column which determine the selected participants.
 
+    Returns dates, start times, end times, locations and dataframe of filtered participants whose
+    details are to be added into google calendar.
     '''
 
     if filter_column is not None and select is not None:
@@ -50,7 +91,6 @@ def extract_info(participants, date_col, time_col, location_col,
             t_new = t.replace('.', ':')  # can only detect time with colon
             timings = np.append(timings, t_new)
 
-
     start_points = np.array([])
     end_points = np.array([])
     for i in timings:
@@ -60,7 +100,7 @@ def extract_info(participants, date_col, time_col, location_col,
         start_points = np.append(start_points, start)
         end_points = np.append(end_points, end)
 
-    return date, start_points, end_points, location
+    return date, start_points, end_points, location, to_add
 
 
 def create_event(event_name, description, date, start, end, location, timezone, creator_email,
@@ -73,7 +113,7 @@ def create_event(event_name, description, date, start, end, location, timezone, 
 
     event = {
       'summary': event_name,
-      'colorId': '9', #blue
+      'colorId': '9',  #blue
       'kind': 'calendar#event',
       'location': location,
       'description': description,
@@ -102,55 +142,49 @@ def create_event(event_name, description, date, start, end, location, timezone, 
 # =============================================================================
 
 
-def main():
 
-    # Set up credentials
-    scopes = ['https://www.googleapis.com/auth/calendar']
-    flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", scopes=scopes)
-    credentials = flow.run_console()  # need to manually enter authorization code here
+def main(silent=False):
 
-    pickle.dump(credentials, open("token.pkl", "wb"))
-    credentials = pickle.load(open("token.pkl", "rb"))
-    service = build("calendar", "v3", credentials=credentials)
+    # Set up OAuth
+    service = setup_oath()
 
-    # Get details
+    # Get all participants details
     participants = preprocess_file(file='C:/Users/Zen Juen/Dropbox/Deception_MockCrime/Deception_MockCrime/Participants Scheduling/Master_Participant_List.xlsx')
 
-    # Modify column name entries as per your input file
-    dates, start_points, end_points, locations = extract_info(participants,
-                                                              date_col='Date_Session1',
-                                                              time_col='Timeslot_Session1',
-                                                              location_col='Location_Session1',
-                                                              filter_column='Calendar_Event',
-                                                              select='No')
-        # select as No because it represents that the 'Calendar_Event' has not been added yet.
+    # Get scheduled details
+    dates, start_points, end_points, locations, to_add = extract_info(participants,
+                                                                      date_col='Date_Session1',
+                                                                      time_col='Timeslot_Session1',
+                                                                      location_col='Location_Session1',
+                                                                      filter_column='Calendar_Event',
+                                                                      select='No')
+    # select as No because it represents that the 'Calendar_Event' has not been added yet.
 
     # Create event
     if len(dates) > 1: # if more than one participant to add:
         events = []
-        calendar_ids = []
         for date, start, end, location in zip(dates, start_points, end_points, locations):
             event, calendar_id = create_event(event_name='fMRI study Session 1', description='',
-                                      date=date, start=start, end=end, location=location,
-                                      timezone='Asia/Singapore',
-                                      creator_email='decisiontask.study@gmail.com',
-                                      calendar_id='Lab Use (NTU)')
+                                              date=date, start=start, end=end, location=location,
+                                              timezone='Asia/Singapore',
+                                              creator_email='decisiontask.study@gmail.com',
+                                              calendar_id='Lab Use (NTU)')
             events.append(event)
     else:
         event, calendar_id = create_event(event_name='fMRI study Session 1', description='',
-                                          date=date, start=start_points, end=end_points,
+                                          date=dates, start=start_points, end=end_points,
                                           location=locations,
                                           timezone='Asia/Singapore',
                                           creator_email='decisiontask.study@gmail.com',
                                           calendar_id='Lab Use (NTU)')
 
     # Execute
-    ## If 'calendar_id' in `create_event` is set to primary, then use primary calendar, if not
-    ## input the string of the calendar name that you intend to use.
+    # If 'calendar_id' in `create_event` is set to primary, then use primary calendar, if not
+    # input the string of the calendar name that you intend to use.
     result = service.calendarList().list().execute()
     if calendar_id != 'primary':
         choose = []
-        for dictionary in result['items']:  # for name, age in dictionary.iteritems():  (for Python 2.x)
+        for dictionary in result['items']:
             if dictionary['summary'] == calendar_id:
                 choose.append(dictionary['id'])
         calendar_id = choose[0]
@@ -163,7 +197,18 @@ def main():
     else:
         service.events().insert(calendarId=calendar_id, body=event).execute()
 
+    # Print output
+    if not silent:
+        for name in to_add['Participant Name']:
+            info_date = to_add['Date_Session1'][to_add['Participant Name'] == name].iloc[0].date().strftime("%d-%m-%Y")
+            info_time = to_add['Timeslot_Session1'][to_add['Participant Name'] == name].iloc[0]
+            info_location = to_add['Location_Session1'][to_add['Participant Name'] == name].iloc[0]
+
+            print('Adding calendar event for ' + f'{name} ' + 'at ' + f'{info_date}, '
+                  + f'{info_time}, ' + f'{info_location} ')
+
+
 
 
 if __name__ == "__main__":
-    main()
+    main(silent=False)
